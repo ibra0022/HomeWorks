a8125e2afa8c46622e6fde8f582ba22dfb0b0609
ibra0022 <82455736+ibra0022@users.noreply.github.com>
2021-04-15 01:37:38 +0300

    Add files via upload

---
+++ Tokenizer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace tiwAccounts
+{
+    class Program
+    {
+
+        static void tokenizer(string value)
+        {
+            int i = 0;
+
+            int numOfAccounts = 0;
+
+           // Console.WriteLine("0");
+
+
+            while(i < value.Length)
+            {
+                //Console.WriteLine("1");
+
+                if (value[i].Equals('@'))
+                {
+                    //Console.WriteLine("2");
+                    string account = "";
+
+                    numOfAccounts++;
+                    i++;
+
+                    while(i < value.Length)
+                    {
+                        account += value[i];
+
+                        if(value[i].Equals(' '))
+                        {
+                            break;
+                        }
+
+                        i++;
+                    }
+                    Console.WriteLine("the account number " + numOfAccounts + " is " +account);
+                }
+
+                i++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("the total number of all accounts is " + numOfAccounts);
+        }
+
+        static void Main(string[] args)
+        {
+
+            tokenizer("sdlkjghdflkjsg sdflkjhglsd gjlf sldf @lkjdghsdl ldsjhgfklh @ls fdlghjkfh @asdf asdasf @jjg");
+        }
+    }
+}
